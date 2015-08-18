253bb1f828a4add7348406d52a96df8f9190c2cb
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-08-18 21:21:41 +0200

    move comment

--- pubrec.go
+++ pubrec.go
@@ -14,12 +14,12 @@
 
 package message
 
+// A PUBREC Packet is the response to a PUBLISH Packet with QoS 2. It is the second
+// packet of the QoS 2 protocol exchange.
 type PubrecMessage struct {
 	PubackMessage
 }
 
-// A PUBREC Packet is the response to a PUBLISH Packet with QoS 2. It is the second
-// packet of the QoS 2 protocol exchange.
 var _ Message = (*PubrecMessage)(nil)
 
 // NewPubrecMessage creates a new PUBREC message.
