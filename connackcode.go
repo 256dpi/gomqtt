be770b2e185ec21f7f089bbbbc437763434ab85e
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-08-31 21:17:21 +0200

    fix typos and comments

--- connackcode.go
+++ connackcode.go
@@ -15,40 +15,40 @@
 package message
 
 // ConnackCode is the type representing the return code in the CONNACK message,
-// returned after the initial CONNECT message
+// returned after the initial CONNECT message.
 type ConnackCode byte
 
 const (
-	// Connection accepted
+	// The server has accepted the connection.
 	ConnectionAccepted ConnackCode = iota
 
-	// The Server does not support the level of the MQTT protocol requested by the Client
+	// The Server does not support the level of the MQTT protocol requested by the Client.
 	ErrInvalidProtocolVersion
 
-	// The Client identifier is correct UTF-8 but not allowed by the server
+	// The Client identifier is correct UTF-8 but not allowed by the server.
 	ErrIdentifierRejected
 
-	// The Network Connection has been made but the MQTT service is unavailable
+	// The Network Connection has been made but the MQTT service is unavailable.
 	ErrServerUnavailable
 
-	// The data in the user name or password is malformed
+	// The data in the user name or password is malformed.
 	ErrBadUsernameOrPassword
 
-	// The Client is not authorized to connect
+	// The Client is not authorized to connect.
 	ErrNotAuthorized
 )
 
-// Value returns the value of the ConnackCode, which is just the byte representation
+// Value returns the value of the ConnackCode, which is just the byte representation.
 func (this ConnackCode) Value() byte {
 	return byte(this)
 }
 
-// Valid checks to see if the ConnackCode is valid. Currently valid codes are <= 5
+// Valid checks to see if the ConnackCode is valid. Currently valid codes are <= 5.
 func (this ConnackCode) Valid() bool {
 	return this <= 5
 }
 
-// Error returns the corresonding error string for the ConnackCode
+// Error returns the corresponding error string for the ConnackCode.
 func (this ConnackCode) Error() string {
 	switch this {
 	case 0:
