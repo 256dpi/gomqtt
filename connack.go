35a95e5a474e1733ceac02b87135334c4b3c1de1
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-12-06 18:48:20 +0100

    these values should be self explanatory

--- connack.go
+++ connack.go
@@ -19,23 +19,13 @@ import "fmt"
 // ConnackCode is the type representing the return code in the CONNACK message.
 type ConnackCode byte
 
+// All existing ConnackCodes.
 const (
-	// The server has accepted the connection.
 	ConnectionAccepted ConnackCode = iota
-
-	// The Server does not support the level of the MQTT protocol requested by the Client.
 	ErrInvalidProtocolVersion
-
-	// The Client identifier is correct UTF-8 but not allowed by the server.
 	ErrIdentifierRejected
-
-	// The Network Connection has been made but the MQTT service is unavailable.
 	ErrServerUnavailable
-
-	// The data in the user name or password is malformed.
 	ErrBadUsernameOrPassword
-
-	// The Client is not authorized to connect.
 	ErrNotAuthorized
 )
 
