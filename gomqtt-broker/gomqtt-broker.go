ce62cfd435ca85ecbc01dc20784cbc7b41ebd047
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-12-16 01:34:22 +0100

    renamed

--- gomqtt-broker/gomqtt-broker.go
+++ gomqtt-broker/gomqtt-broker.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2014 The gomqtt Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -11,8 +25,11 @@ import (
 
 	"github.com/gomqtt/server"
 	"github.com/gomqtt/broker"
+	"net"
 )
 
+var host = flag.String("host", "0.0.0.0", "broker host")
+var port = flag.String("port", "1884", "broker port")
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 var memprofile = flag.String("memprofile", "", "write memory profile to this file")
 
@@ -41,7 +58,7 @@ func main() {
 //	b.RetainedBackend = m
 
 	s := server.NewServer(b.Handle)
-	s.LaunchTCPConfiguration("0.0.0.0:1884")
+	s.LaunchTCPConfiguration(net.JoinHostPort(*host, *port))
 
 	fmt.Println("started!")
 
--- profile.sh
+++ profile.sh
@@ -2,16 +2,16 @@
 
 mkdir -p ./profile
 
-echo "building gomqttb for linux..."
-env GOOS=linux GOARCH=amd64 go build -o ./profile/gomqttb ./gomqttb/gomqttb.go
+echo "building gomqtt-broker for linux..."
+env GOOS=linux GOARCH=amd64 go build -o ./profile/gomqtt-broker ./gomqtt-broker/gomqtt-broker.go
 
-echo "running gomqttb..."
-vagrant ssh -c '/vagrant/profile/gomqttb -cpuprofile /vagrant/profile/cpu.prof -memprofile /vagrant/profile/mem.prof'
+echo "running gomqtt-broker..."
+vagrant ssh -c '/vagrant/profile/gomqtt-broker -cpuprofile /vagrant/profile/cpu.prof -memprofile /vagrant/profile/mem.prof'
 
 echo "generating cpu callgraph..."
-go tool pprof --pdf ./profile/gomqttb ./profile/cpu.prof > ./profile/cpu.pdf
+go tool pprof --pdf ./profile/gomqtt-broker ./profile/cpu.prof > ./profile/cpu.pdf
 open ./profile/cpu.pdf
 
 echo "generating mem callgraph..."
-go tool pprof --pdf ./profile/gomqttb ./profile/mem.prof > ./profile/mem.pdf
+go tool pprof --pdf ./profile/gomqtt-broker ./profile/mem.prof > ./profile/mem.pdf
 open ./profile/mem.pdf
