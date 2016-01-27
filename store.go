0f9650a8b3c91ea7558400d693d83dc348240017
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-01-27 23:28:02 +0100

    first memory store implementation

--- store.go
+++ store.go
@@ -14,7 +14,11 @@
 
 package client
 
-import "github.com/gomqtt/packet"
+import (
+	"sync"
+
+	"github.com/gomqtt/packet"
+)
 
 // A store is used to persists incoming and outgoing packets until they are
 // successfully acknowledged by the other side.
@@ -23,17 +27,83 @@ type Store interface {
 	Open() error
 
 	// Put will persist a packet to the store.
-	Put(*packet.Packet) error
+	Put(packet.Packet) error
 
-	// Dell will remove a packet from the store.
-	Del(*packet.Packet) error
+	// Get will retrieve a packet from the store.
+	Get(uint16) (packet.Packet, error)
+
+	// Del will remove a packet from the store.
+	Del(uint16) error
 
 	// All will return all packets currently in the store.
 	All() (error, []packet.Packet)
 
-	// Close will close the store.
-	Close() error
-
 	// Reset will completely reset the store.
 	Reset() error
+
+	// Close will close the store.
+	Close() error
+}
+
+type MemoryStore struct {
+	store map[uint16]packet.Packet
+	mutex sync.Mutex
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		store: make(map[uint16]packet.Packet),
+	}
+}
+
+func (s *MemoryStore) Open() error {
+	return nil
+}
+
+func (s *MemoryStore) Put(pkt packet.Packet) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	id, ok := packet.PacketID(pkt)
+	if ok {
+		s.store[id] = pkt
+	}
+
+	return nil
+}
+
+func (s *MemoryStore) Get(id uint16) (packet.Packet, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	return s.store[id], nil
+}
+
+func (s *MemoryStore) Del(id uint16) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	delete(s.store, id)
+
+	return nil
+}
+
+func (s *MemoryStore) All() (error, []packet.Packet) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	return nil, nil
+}
+
+func (s *MemoryStore) Reset() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.store = make(map[uint16]packet.Packet)
+
+	return nil
+}
+
+func (s *MemoryStore) Close() error {
+	return nil
 }
