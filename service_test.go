a1f0bc481585db7728d738d7d3ec9e1947c675b1
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-02-08 16:58:34 +0100

    added pub sub test

--- service_test.go
+++ service_test.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/gomqtt/flow"
 	"github.com/gomqtt/packet"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestClearSession(t *testing.T){
@@ -34,6 +35,64 @@ func TestClearSession(t *testing.T){
 	done, tp := fakeBroker(t, broker)
 
 	ClearSession(tp.url(), "test")
+
+	<-done
+}
+
+func TestServicePublishSubscribe(t *testing.T) {
+	subscribe := packet.NewSubscribePacket()
+	subscribe.Subscriptions = []packet.Subscription{
+		{Topic: []byte("test")},
+	}
+	subscribe.PacketID = 0
+
+	suback := packet.NewSubackPacket()
+	suback.ReturnCodes = []byte{0}
+	suback.PacketID = 0
+
+	publish := packet.NewPublishPacket()
+	publish.Topic = []byte("test")
+	publish.Payload = []byte("test")
+
+	broker := flow.New().
+		Receive(connectPacket()).
+		Send(connackPacket(packet.ConnectionAccepted)).
+		Receive(subscribe).
+		Send(suback).
+		Receive(publish).
+		Send(publish).
+		Receive(disconnectPacket()).
+		End()
+
+	done, tp := fakeBroker(t, broker)
+
+	wait := make(chan struct{})
+
+	s := NewService()
+
+	s.Online = func(resumed bool) {
+		assert.False(t, resumed)
+	}
+
+	s.Offline = func() {
+
+	}
+
+	s.Message = func(topic string, payload []byte) {
+		assert.Equal(t, "test", topic)
+		assert.Equal(t, []byte("test"), payload)
+		close(wait)
+	}
+
+	s.Start(tp.url(), nil)
+
+	s.Subscribe("test", 0).Wait()
+
+	s.Publish("test", []byte("test"), 0, false)
+
+	<-wait
+
+	s.Stop()
 
 	<-done
 }
