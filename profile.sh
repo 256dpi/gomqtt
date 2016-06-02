02e25960d4401f619f10b0b31b28d6cd357626ff
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-06-02 14:50:07 +0200

    removed linux profiling stuff

--- profile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ./profile
-
-echo "building gomqtt-broker for linux..."
-env GOOS=linux GOARCH=amd64 go build -o ./profile/gomqtt-broker ./gomqtt-broker/gomqtt-broker.go
-
-echo "running gomqtt-broker..."
-vagrant ssh -c '/vagrant/profile/gomqtt-broker -cpuprofile /vagrant/profile/cpu.prof -memprofile /vagrant/profile/mem.prof'
