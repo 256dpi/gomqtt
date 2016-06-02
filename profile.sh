6da2a23aa743e12fba0675239aff22bf19f6a5a4
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-06-02 13:00:18 +0200

    cleanup

--- profile.sh
+++ profile.sh
@@ -7,4 +7,3 @@ env GOOS=linux GOARCH=amd64 go build -o ./profile/gomqtt-broker ./gomqtt-broker/
 
 echo "running gomqtt-broker..."
 vagrant ssh -c '/vagrant/profile/gomqtt-broker -cpuprofile /vagrant/profile/cpu.prof -memprofile /vagrant/profile/mem.prof'
-
