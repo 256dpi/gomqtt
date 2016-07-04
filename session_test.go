52a1ea4e625fb69d172ec89fa201d083991890df
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-07-04 14:34:05 +0200

    simplify tests

--- session_test.go
+++ session_test.go
@@ -14,10 +14,76 @@
 
 package client
 
-import "testing"
+import (
+	"math"
+	"testing"
 
-func TestMemorySession(t *testing.T) {
-	SessionTest(t, func() Session {
-		return NewMemorySession()
-	})
+	"github.com/gomqtt/packet"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemorySessionPacketID(t *testing.T) {
+	session := NewMemorySession()
+
+	assert.Equal(t, uint16(1), session.PacketID())
+	assert.Equal(t, uint16(2), session.PacketID())
+
+	for i := 0; i < math.MaxUint16-3; i++ {
+		session.PacketID()
+	}
+
+	assert.Equal(t, uint16(math.MaxUint16), session.PacketID())
+	assert.Equal(t, uint16(1), session.PacketID())
+
+	err := session.Reset()
+	assert.NoError(t, err)
+
+	assert.Equal(t, uint16(1), session.PacketID())
+}
+
+func TestMemorySessionPacketStore(t *testing.T) {
+	session := NewMemorySession()
+
+	publish := packet.NewPublishPacket()
+	publish.PacketID = 1
+
+	pkt, err := session.LookupPacket(incoming, 1)
+	assert.NoError(t, err)
+	assert.Nil(t, pkt)
+
+	err = session.SavePacket(incoming, publish)
+	assert.NoError(t, err)
+
+	pkt, err = session.LookupPacket(incoming, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, publish, pkt)
+
+	pkts, err := session.AllPackets(incoming)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(pkts))
+
+	err = session.DeletePacket(incoming, 1)
+	assert.NoError(t, err)
+
+	pkt, err = session.LookupPacket(incoming, 1)
+	assert.NoError(t, err)
+	assert.Nil(t, pkt)
+
+	pkts, err = session.AllPackets(incoming)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(pkts))
+
+	err = session.SavePacket(outgoing, publish)
+	assert.NoError(t, err)
+
+	pkts, err = session.AllPackets(outgoing)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(pkts))
+
+	err = session.Reset()
+	assert.NoError(t, err)
+
+	pkts, err = session.AllPackets(outgoing)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(pkts))
 }
