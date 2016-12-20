d499ae71b5eb2eb80c53f66b7321a34b77b07d3b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-12-20 13:35:22 +0100

    added ReceiveMessage tool

--- tools.go
+++ tools.go
@@ -98,3 +98,54 @@ func PublishMessage(opts *Options, msg *packet.Message) error {
 	// disconnect
 	return client.Disconnect()
 }
+
+func ReceiveMessage(opts *Options, topic string, qos byte) (*packet.Message, error) {
+	client := New()
+
+	// copy options
+	opts = opts.Copy()
+	opts.CleanSession = true
+
+	// connect to broker
+	future, err := client.Connect(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	// wait for connack
+	future.Wait()
+
+	// check if connection has been accepted
+	if future.ReturnCode != packet.ConnectionAccepted {
+		return nil, ErrClientConnectionDenied
+	}
+
+	// create channel
+	msgCh := make(chan *packet.Message)
+	errCh := make(chan error)
+
+	// set callback
+	client.Callback = func(msg *packet.Message, err error) {
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		msgCh <- msg
+	}
+
+	// make subscription
+	_, err = client.Subscribe(topic, qos)
+	if err != nil {
+		return nil, err
+	}
+
+	// wait
+	select{
+	case msg := <- msgCh:
+		// disconnect
+		return msg, client.Disconnect()
+	case err := <- errCh:
+		return nil, err
+	}
+}
--- tools_test.go
+++ tools_test.go
@@ -51,7 +51,7 @@ func TestPublishMessage(t *testing.T) {
 	publish := packet.NewPublishPacket()
 	publish.Message = packet.Message{
 		Topic: "test",
-		Payload: []byte("foo"),
+		Payload: []byte("test"),
 		Retain: true,
 	}
 
@@ -66,6 +66,42 @@ func TestPublishMessage(t *testing.T) {
 
 	err := PublishMessage(NewOptions(port.URL()), &publish.Message)
 	assert.NoError(t, err)
+
+	<-done
+}
+
+func TestReceiveMessage(t *testing.T) {
+	subscribe := packet.NewSubscribePacket()
+	subscribe.PacketID = 1
+	subscribe.Subscriptions = []packet.Subscription{
+		{ Topic: "test" },
+	}
+
+	suback := packet.NewSubackPacket()
+	suback.PacketID = 1
+	suback.ReturnCodes = []uint8{0}
+
+	publish := packet.NewPublishPacket()
+	publish.Message = packet.Message{
+		Topic: "test",
+		Payload: []byte("test"),
+		Retain: true,
+	}
+
+	broker := tools.NewFlow().
+		Receive(connectPacket()).
+		Send(connackPacket()).
+		Receive(subscribe).
+		Send(suback).
+		Send(publish).
+		Receive(disconnectPacket()).
+		End()
+
+	done, port := fakeBroker(t, broker)
+
+	msg, err := ReceiveMessage(NewOptions(port.URL()), "test", 0)
+	assert.NoError(t, err)
+	assert.Equal(t, publish.Message.String(), msg.String())
 
 	<-done
 }
