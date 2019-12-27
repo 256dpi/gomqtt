01c79eb135c9bc052e12bccb4ee49ef617c84188
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-12-27 19:26:36 +0100

    internalize mutex

--- client/tracker.go
+++ client/tracker.go
@@ -7,11 +7,10 @@ import (
 
 // A Tracker keeps track of keep alive intervals.
 type Tracker struct {
-	sync.RWMutex
-
 	last    time.Time
 	pings   uint8
 	timeout time.Duration
+	mutex   sync.RWMutex
 }
 
 // NewTracker returns a new tracker.
@@ -24,40 +23,48 @@ func NewTracker(timeout time.Duration) *Tracker {
 
 // Reset will reset the tracker.
 func (t *Tracker) Reset() {
-	t.Lock()
-	defer t.Unlock()
+	// acquire mutex
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
 
+	// reset timestamp
 	t.last = time.Now()
 }
 
 // Window returns the time until a new ping should be sent.
 func (t *Tracker) Window() time.Duration {
-	t.RLock()
-	defer t.RUnlock()
+	// acquire mutex
+	t.mutex.RLock()
+	defer t.mutex.RUnlock()
 
 	return t.timeout - time.Since(t.last)
 }
 
 // Ping marks a ping.
 func (t *Tracker) Ping() {
-	t.Lock()
-	defer t.Unlock()
+	// acquire mutex
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
 
+	// increment
 	t.pings++
 }
 
 // Pong marks a pong.
 func (t *Tracker) Pong() {
-	t.Lock()
-	defer t.Unlock()
+	// acquire mutex
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
 
+	// decrement
 	t.pings--
 }
 
 // Pending returns if pings are pending.
 func (t *Tracker) Pending() bool {
-	t.RLock()
-	defer t.RUnlock()
+	// acquire mutex
+	t.mutex.RLock()
+	defer t.mutex.RUnlock()
 
 	return t.pings > 0
 }
