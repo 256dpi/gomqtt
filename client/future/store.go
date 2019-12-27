cb7a6d5c308e591e1adffa83caef3064365ceea3
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-12-27 17:38:08 +0100

    unexpose mutex

--- client/future/store.go
+++ client/future/store.go
@@ -9,10 +9,9 @@ import (
 
 // A Store is used to store futures.
 type Store struct {
-	sync.RWMutex
-
 	protected bool
 	store     map[packet.ID]*Future
+	mutex     sync.RWMutex
 }
 
 // NewStore will create a new Store.
@@ -24,35 +23,41 @@ func NewStore() *Store {
 
 // Put will save a future to the store.
 func (s *Store) Put(id packet.ID, future *Future) {
-	s.Lock()
-	defer s.Unlock()
+	// acquire mutex
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
+	// set future
 	s.store[id] = future
 }
 
 // Get will retrieve a future from the store.
 func (s *Store) Get(id packet.ID) *Future {
-	s.RLock()
-	defer s.RUnlock()
+	// acquire mutex
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 
 	return s.store[id]
 }
 
 // Delete will remove a future from the store.
 func (s *Store) Delete(id packet.ID) {
-	s.Lock()
-	defer s.Unlock()
+	// acquire mutex
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
+	// delete future
 	delete(s.store, id)
 }
 
 // All will return a slice with all stored futures.
 func (s *Store) All() []*Future {
-	s.RLock()
-	defer s.RUnlock()
+	// acquire mutex
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 
+	// collect futures
 	all := make([]*Future, 0, len(s.store))
-
 	for _, savedFuture := range s.store {
 		all = append(all, savedFuture)
 	}
@@ -63,38 +68,45 @@ func (s *Store) All() []*Future {
 // Protect will set the protection attribute and if true prevents the store from
 // being cleared.
 func (s *Store) Protect(value bool) {
-	s.Lock()
-	defer s.Unlock()
+	// acquire mutex
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
+	// set flag
 	s.protected = value
 }
 
 // Clear will cancel all stored futures and remove them if the store is unprotected.
 func (s *Store) Clear() {
-	s.Lock()
-	defer s.Unlock()
+	// acquire mutex
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
+	// check flag
 	if s.protected {
 		return
 	}
 
+	// cancel all futures
 	for _, savedFuture := range s.store {
 		savedFuture.Cancel()
 	}
 
+	// reset store
 	s.store = make(map[packet.ID]*Future)
 }
 
-// Await will wait until all futures have completed and removed or timeout is
-// reached.
+// Await will wait until all futures have completed or cancelled, or the timeout
+// has been reached.
 func (s *Store) Await(timeout time.Duration) error {
-	stop := time.Now().Add(timeout)
+	// prepare deadline
+	deadline := time.Now().Add(timeout)
 
 	for {
-		// Get futures
-		s.RLock()
+		// get futures
+		s.mutex.RLock()
 		futures := s.All()
-		s.RUnlock()
+		s.mutex.RUnlock()
 
 		// return if no futures are left
 		if len(futures) == 0 {
@@ -102,7 +114,7 @@ func (s *Store) Await(timeout time.Duration) error {
 		}
 
 		// wait for next future to complete
-		err := futures[0].Wait(stop.Sub(time.Now()))
+		err := futures[0].Wait(deadline.Sub(time.Now()))
 		if err != nil {
 			return err
 		}
